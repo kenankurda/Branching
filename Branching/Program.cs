939f00fbab3d13671ffcb19a87f2c0d396d2ba34
Kenan Kurda <KenanK@intecbrussel.intra>
2020-10-08 08:13:48 +0200

    Add project files.

---
+++ Branching/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef het x-coördinaat van de linker bovenhoek van de rechthoek");
+            int xPos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef het y-coördinaat van de linker bovenhoek van de rechthoek");
+            int yPos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef de breedte van de rechthoek");
+            int breedte = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef de hoogte van de rechthoek");
+            int hoogte = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef de x-coördinaat van het te testen punt");
+            int puntX = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef de y-coördinaat van het te testen punt");
+            int puntY = int.Parse(Console.ReadLine());
+            if (puntX > xPos && puntX < puntX + breedte && puntY > yPos && puntY < puntY + hoogte)
+            {
+                Console.WriteLine("Het punt ligt binnen de gegeven rechthoek");
+            }
+            else {
+                Console.WriteLine("Het punt ligt niet binnen de gegeven rechthoek");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OverlapRanges/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverlapRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            Console.WriteLine("Geef een getal a (ondergrens bereik 1)");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef een getal b (bovengrens bereik 1)");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef een getal c (ondergrens bereik 2)");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef een getal d (bovengrens bereik 2)");
+            d = int.Parse(Console.ReadLine());
+            if (c >= a && d <= b)
+                Console.WriteLine("Het bereik gaande van c tot d ligt binnen het bereik gaande van a tot b");
+            else
+                Console.WriteLine("Het bereik gaande van c tot d ligt niet binnen het bereik gaande van a tot b");
+
+            Console.ReadLine();
+
+        }
+    }
+}
